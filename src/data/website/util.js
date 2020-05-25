0d4ecacccee84d6c291f921f01b042b868c6d57f
cuifuan <cuifuan@MacBook-Pro.local>
2020-05-25 14:44:05 +0800

    更新一些工具

---
+++ src/data/website/util.js
@@ -0,0 +1,30 @@
+export default {
+    title: '实用工具',
+    name: 'utils',
+    nav: [
+        {
+            icon: './static/images/aconvert.png',
+            name: 'Aconvert',
+            desc: '在线转换各类PDF，文档，电子书，图像，图标，视频，音频和压缩文件',
+            link: 'https://www.aconvert.com/cn/'
+        },
+        {
+            icon: './static/images/onlineocr.png',
+            name: 'onlineocr',
+            desc: '免费在线OCR文字识别,将扫描的PDF文档转换为WORD文档',
+            link: 'https://www.onlineocr.net/zh_hans/'
+        },
+        {
+            icon: './static/images/51ape.png',
+            name: '📀无损歌曲下载',
+            desc: '免费在线OCR文字识别,将扫描的PDF文档转换为WORD文档',
+            link: 'http://www.51ape.com/'
+        },
+        {
+            icon: './static/images/yotobe.png',
+            name: 'youtobe下载(需扶墙)',
+            desc: '免费在线OCR文字识别,将扫描的PDF文档转换为WORD文档',
+            link: 'https://zh.savefrom.net/7/'
+        },
+    ]
+}
